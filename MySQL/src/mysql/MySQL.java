73635e2310d641c4982151cb627c69ee3b234aaa
jeroen <jeroen@MP-Satellite>
2013-12-26 16:16:41 +0100

    CommitTest3

--- MySQL/src/mysql/MySQL.java
+++ MySQL/src/mysql/MySQL.java
@@ -1,7 +1,9 @@
 //<editor-fold defaultstate="collapsed" desc="Jibberish">
 package mysql;
 
+import java.sql.*;
 import java.util.ArrayList;
+import java.util.logging.*;
 //</editor-fold>
 
 /**
@@ -12,19 +14,80 @@ import java.util.ArrayList;
 public class MySQL {
 
     //<editor-fold defaultstate="collapsed" desc="Declarations">
-    //TODO
+    Connection con;
     //</editor-fold>
-
+    
     //<editor-fold defaultstate="collapsed" desc="Constructor">
     /**
-     * In this class //TODO
+     * In this method the connection with the database will be initiated.
      */
-     public MySQL(){
-
-     }
+    public MySQL(String server, String database, String user, String password) {
+        String url = "jdbc:mysql://" + server + ":3306/" + database;
+        try {
+            con = DriverManager.getConnection(url, user, password);
+        } catch (SQLException ex) {
+            Logger lgr = Logger.getLogger(MySQL.class.getName());
+            lgr.log(Level.SEVERE, ex.getMessage(), ex);
+        } finally {
+            try {
+                if (con != null) {
+                    con.close();
+                }
+            } catch (SQLException ex) {
+                Logger lgr = Logger.getLogger(MySQL.class.getName());
+                lgr.log(Level.SEVERE, ex.getMessage(), ex);
+            }
+        }
+    }
+    //</editor-fold>
+    
+    //<editor-fold desc="Functions">
+    //<editor-fold defaultstate="collapsed" desc="Set SQL">
+    /**
+     * In this method the statement will be executed on the server.
+     *
+     * @param sql The statement that the server must execute.
+     */
+    public void setSQL(String sql, ArrayList<String> parameters) {
+        PreparedStatement pst = null;
+        
+        try {
+            pst = con.prepareStatement(sql);
+            for (int i = 1; i <= parameters.size(); i++) {
+                pst.setString(i, parameters.get(i));
+            }
+            pst.executeUpdate();
+        } catch (SQLException ex) {
+            Logger lgr = Logger.getLogger(MySQL.class.getName());
+            lgr.log(Level.SEVERE, ex.getMessage(), ex);
+        } finally {
+            try {
+                if (pst != null) {
+                    pst.close();
+                }
+                if (con != null) {
+                    con.close();
+                }
+            } catch (SQLException ex) {
+                Logger lgr = Logger.getLogger(MySQL.class.getName());
+                lgr.log(Level.SEVERE, ex.getMessage(), ex);
+            }
+        }
+    }
     //</editor-fold>
 
-    //<editor-fold desc="Functions">
-    //TODO
+    //<editor-fold defaultstate="collapsed" desc="Get SQL">
+    /**
+     * This returned a list of ArratList with the data it gets back from the
+     * deck.
+     *
+     * @param sql
+     * @return
+     */
+    public ArrayList<String> getSQL(String sql) {
+        ArrayList<String> returner = null;
+        return returner;
+    }
+    //</editor-fold>
     //</editor-fold>
 }
