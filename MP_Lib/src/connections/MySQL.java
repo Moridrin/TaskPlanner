b80324a0e2910219259e60891d3eabd1ba356e19
jeroen <jeroen@MP-Satellite>
2013-12-28 21:46:41 +0100

    I made the getSQL fully functional. For as far as I can see it is now fully functional, and fully compatible.

--- MP_Lib/src/connections/MySQL.java
+++ MP_Lib/src/connections/MySQL.java
@@ -13,7 +13,7 @@ import java.util.logging.*;
  *
  * @author jeroen
  */
-public class MySQL implements Serializable{
+public class MySQL implements Serializable {
 
     //<editor-fold defaultstate="collapsed" desc="Declarations">
     private Connection con;
@@ -24,7 +24,14 @@ public class MySQL implements Serializable{
 
     //<editor-fold defaultstate="collapsed" desc="Constructor">
     /**
-     * In this method the connection with the database will be initiated.
+     * In this Constructor the required variables for the connection with the
+     * database will be initiated. * @param server
+     *
+     * @param port is the port where the server listens to for Database
+     * Connections.
+     * @param database is the name of the database.
+     * @param user is the user that is used to login.
+     * @param password the password that is associated with the user.
      */
     public MySQL(String server, int port, String database, String user, String password) {
         this.url = "jdbc:mysql://" + server + ":" + port + "/" + database;
@@ -34,19 +41,15 @@ public class MySQL implements Serializable{
     //</editor-fold>
 
     //<editor-fold desc="Functions">
-    /*
-    //<editor-fold defaultstate="collapsed" desc="Add Observer">
-    public void addObserver(Object observer){
-        this.addObserver(observer);
-    }
-    //</editor-fold>
-    */
-    
     //<editor-fold defaultstate="collapsed" desc="Set SQL">
     /**
      * In this method the statement will be executed on the server.
      *
-     * @param sql The statement that the server must execute.
+     * @param sql is the statement that the server must execute.
+     * @param parameters contains a list with parameters that are send to the
+     * server along side the statement.
+     * @throws SQLException it throws this exception instead of catching it
+     * here, so that the class calling this function knows what the problem is.
      */
     public void setSQL(String sql, ArrayList<String> parameters) throws SQLException {
         PreparedStatement pst = null;
@@ -64,20 +67,49 @@ public class MySQL implements Serializable{
             Logger lgr = Logger.getLogger(MySQL.class.getName());
             lgr.log(Level.SEVERE, ex.getMessage(), ex);
         }
-        //notifyObservers();
     }
     //</editor-fold>
 
     //<editor-fold defaultstate="collapsed" desc="Get SQL">
     /**
-     * This returned a list of ArratList with the data it gets back from the
-     * deck.
+     * This sends an SELECT statement to the Database and returns whatever comes
+     * back.
      *
-     * @param sql
-     * @return
+     * @param sql is the statement that will be send to the Database.
+     * @param columns contains a lost with the names of the columns that you ask
+     * for in the SQL Statement.
+     * @throws SQLException it throws this exception instead of catching it
+     * here, so that the class calling this function knows what the problem is.
+     * @return is a List of String Arrays so the multiple columns can be set in
+     * the Array, and the rows can be set as items in the List.
      */
-    public ArrayList<String> getSQL(String sql) {
-        ArrayList<String> returner = null;
+    public ArrayList<String[]> getSQL(String sql, ArrayList<String> columns) throws SQLException {
+        ArrayList<String[]> returner = new ArrayList<>();
+        PreparedStatement pst = null;
+        ResultSet resultSet;
+        con = DriverManager.getConnection(url, user, password);
+        for (int i = 0; i < columns.size(); i++) {
+            //String replaceString = "?" + i;
+            //sql.replace(replaceString, columns.get(i));
+            sql = sql.replaceFirst("\\?", columns.get(i));
+        }
+        pst = con.prepareStatement(sql);
+        resultSet = pst.executeQuery();
+        while (resultSet.next()) {
+            String[] row = new String[columns.size()];
+            for (int i = 0; i < columns.size(); i++) {
+                row[i] = resultSet.getString(columns.get(i));
+            }
+            returner.add(row);
+        }
+        try {
+            if (con != null) {
+                con.close();
+            }
+        } catch (SQLException ex) {
+            Logger lgr = Logger.getLogger(MySQL.class.getName());
+            lgr.log(Level.SEVERE, ex.getMessage(), ex);
+        }
         return returner;
     }
     //</editor-fold>
