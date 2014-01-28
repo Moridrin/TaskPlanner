45ba12fa06fa8437936d2658ba14aff308c3a85c
jeroen <jeroen@MP-Satellite>
2014-01-28 15:35:57 +0100

    Layout updated.

--- MP_Lib/src/exceptions/UnsupportedType.java
+++ MP_Lib/src/exceptions/UnsupportedType.java
@@ -1,3 +1,4 @@
+//<editor-fold defaultstate="collapsed" desc="Jibberish">
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -5,6 +6,7 @@
  */
 
 package exceptions;
+//</editor-fold>
 
 /**
  *
